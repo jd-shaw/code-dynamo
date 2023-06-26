c04c8189d215e81a4a961818ce524ddb7836561e
shaw <jind.shaw@outlook.com>
2023-06-26 11:15:40 +0800

    init code dynamo project

--- dynamo-app/src/main/java/com/shaw/app/CodeDynamoApp.java
+++ dynamo-app/src/main/java/com/shaw/app/CodeDynamoApp.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -15,8 +17,14 @@ import java.text.MessageFormat;
  * @date 2023/6/25
  */
 @Slf4j
+@RestController
 @SpringBootApplication
 public class CodeDynamoApp {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
 
     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext application = SpringApplication.run(CodeDynamoApp.class, args);
@@ -29,7 +37,7 @@ public class CodeDynamoApp {
         String path = env.getProperty("spring.mvc.servlet.path", "");
 
         // 应用信息栏
-        String appInfo = MessageFormat.format("应用 '{0}' 运行成功! \n\t", appName);
+        String appInfo = MessageFormat.format("应用 \"{0}\" 运行成功! \n\t", appName);
         // swagger栏
         String swagger = MessageFormat.format("Swagger文档: \t\thttp://{0}:{1}{2}{3}/doc.html\n\t", host, port, contextPath, path);
 
