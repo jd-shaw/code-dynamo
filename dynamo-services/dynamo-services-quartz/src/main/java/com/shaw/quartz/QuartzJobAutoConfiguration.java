e61c72564a15ca22c76f867dbc32262e0f1cd444
shaw <jind.shaw@outlook.com>
2023-07-05 10:06:21 +0800

    init code dynamo project

---
+++ dynamo-services/dynamo-services-quartz/src/main/java/com/shaw/quartz/QuartzJobAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.shaw.quartz;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author xjd
+ * @date 2023/7/5
+ */
+@ConfigurationPropertiesScan
+@AutoConfiguration
+@EntityScan
+@EnableJpaRepositories
+public class QuartzJobAutoConfiguration {
+
+}
