0db655c1904ecf38a0c73257f25da81cf7f6278a
shaw <jind.shaw@outlook.com>
2023-07-06 11:13:07 +0800

    init code dynamo project

--- dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/controller/SiteMessageController.java
+++ dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/controller/SiteMessageController.java
@@ -1,5 +1,10 @@
 package com.shaw.message.controller;
 
+import com.shaw.commons.rest.PageResult;
+import com.shaw.commons.rest.param.PageParam;
+import com.shaw.message.dto.SiteMessageInfoDto;
+import com.shaw.message.service.SiteMessageService;
+import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +20,14 @@ import lombok.RequiredArgsConstructor;
  * @author shaw
  * @date 2023/6/30
  */
+@Getter
 @Tag(name = "站内信")
 @RestController
 @RequestMapping("/site/message")
 @RequiredArgsConstructor
 public class SiteMessageController {
+
+	private final SiteMessageService siteMessageService;
 
 	@Operation(summary = "获取未读的接收消息条数")
 	@GetMapping("/count-by-receive-not-read")
@@ -27,4 +35,9 @@ public class SiteMessageController {
 		return Res.ok();
 	}
 
+	@Operation(summary = "接收消息分页")
+	@GetMapping("/pageByReceive")
+	public ResResult<PageResult<SiteMessageInfoDto>> pageByReceive(PageParam pageParam, SiteMessageInfoDto query) {
+		return Res.ok();
+	}
 }
