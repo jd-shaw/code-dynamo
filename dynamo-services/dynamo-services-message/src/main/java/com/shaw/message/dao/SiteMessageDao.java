71b3d2282e35f259d8cdf02f3ba5725180334552
shaw <jind.shaw@outlook.com>
2023-07-06 11:13:26 +0800

    init code dynamo project

--- dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/dao/SiteMessageDao.java
+++ dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/dao/SiteMessageDao.java
@@ -1,8 +1,17 @@
 package com.shaw.message.dao;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.shaw.message.entity.SiteMessage;
+
 /**
  * @author shaw
  * @date 2023/6/30
  */
-public class SiteMessageDao {
+public interface SiteMessageDao extends JpaRepositoryImplementation<SiteMessage, String> {
+
+	@Query(value = "select count(*) from site_message", nativeQuery = true)
+	int countByReceiveNotRead();
+
 }
