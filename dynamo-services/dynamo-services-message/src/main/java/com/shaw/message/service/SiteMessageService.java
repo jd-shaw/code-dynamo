132731bbbe28b8613a242b801425a062c904e139
shaw <jind.shaw@outlook.com>
2023-07-08 22:09:36 +0800

    init code dynamo project

--- dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/service/SiteMessageService.java
+++ dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/service/SiteMessageService.java
@@ -1,8 +1,12 @@
 package com.shaw.message.service;
 
+import com.shaw.message.dto.SiteMessageDto;
+
 /**
  * @author shaw
  * @date 2023/6/30
  */
 public interface SiteMessageService {
+
+    SiteMessageDto findById(String id);
 }
