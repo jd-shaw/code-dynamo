9214cd810c10294f02daf4917be12a407426dd52
shaw <jind.shaw@outlook.com>
2023-07-08 22:10:00 +0800

    init code dynamo project

--- dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/service/impl/SiteMessageServiceImpl.java
+++ dynamo-services/dynamo-services-message/src/main/java/com/shaw/message/service/impl/SiteMessageServiceImpl.java
@@ -1,10 +1,43 @@
 package com.shaw.message.service.impl;
 
+import org.springframework.stereotype.Service;
+
+import com.shaw.commons.exception.DataNotExistException;
+import com.shaw.message.dao.SiteMessageDao;
+import com.shaw.message.dto.SiteMessageDto;
+import com.shaw.message.entity.SiteMessage;
 import com.shaw.message.service.SiteMessageService;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author shaw
  * @date 2023/6/30
  */
+@Getter
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class SiteMessageServiceImpl implements SiteMessageService {
+
+	private final SiteMessageDao siteMessageDao;
+
+	/**
+	 * 查询详情
+	 */
+	@Override
+	public SiteMessageDto findById(String id) {
+		return getSiteMessageDao().findById(id).map(SiteMessage::toDto).orElseThrow(DataNotExistException::new);
+	}
+
+	//	/**
+	//	 * 未读消息数量
+	//	 */
+	//	public Integer countByReceiveNotRead(SiteMessageInfoDto query) {
+	//		// Long userId = 0L;
+	//		return getSiteMessageDao().countByReceiveNotRead(SecurityUtil.getUserId());
+	//	}
+
 }
