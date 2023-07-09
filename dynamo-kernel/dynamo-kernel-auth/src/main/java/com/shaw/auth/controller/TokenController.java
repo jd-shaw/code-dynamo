54aa453162f3a8da0f591ddbe83568869b77203c
shaw <jind.shaw@outlook.com>
2023-07-09 15:02:13 +0800

    init code dynamo project

--- dynamo-kernel/dynamo-kernel-auth/src/main/java/com/shaw/auth/controller/TokenController.java
+++ dynamo-kernel/dynamo-kernel-auth/src/main/java/com/shaw/auth/controller/TokenController.java
@@ -7,18 +7,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shaw.auth.endpoint.TokenService;
+import com.shaw.auth.service.TokenService;
 import com.shaw.commons.rest.Res;
 import com.shaw.commons.rest.ResResult;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * @author shaw
  * @date 2023/06/20
  */
+@Getter
 @Tag(name = "认证相关")
 @RestController
 @RequestMapping("/token")
@@ -30,13 +32,13 @@ public class TokenController {
 	@Operation(summary = "普通登录")
 	@PostMapping("/login")
 	public ResResult<String> login(HttpServletRequest request, HttpServletResponse response) {
-		return Res.ok(tokenService.login(request, response));
+		return Res.ok(getTokenService().login(request, response));
 	}
 
 	@Operation(summary = "退出")
 	@PostMapping("/logout")
 	public ResResult<String> logout() {
-		tokenService.logout();
+		getTokenService().logout();
 		return Res.ok();
 	}
 
