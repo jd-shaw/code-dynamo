88e5bc662dfc7567b9e4d4a363dca7d62657dd16
shaw <jind.shaw@outlook.com>
2023-07-09 15:02:34 +0800

    init code dynamo project

--- dynamo-kernel/dynamo-kernel-auth/src/main/java/com/shaw/auth/handler/SaRouteHandler.java
+++ dynamo-kernel/dynamo-kernel-auth/src/main/java/com/shaw/auth/handler/SaRouteHandler.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
-import com.shaw.auth.authentication.RouterCheck;
+import com.shaw.auth.saroute.RouterCheck;
 import com.shaw.auth.exception.RouterCheckException;
 import com.shaw.utils.web.WebServletUtil;
 
