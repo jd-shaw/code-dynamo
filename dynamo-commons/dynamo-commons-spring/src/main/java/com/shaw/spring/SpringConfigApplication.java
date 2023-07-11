28e791f876d57776ef9f476d8fdd5b022dcfb46f
shaw <jind.shaw@outlook.com>
2023-07-11 18:31:37 +0800

    add spring commons

--- dynamo-commons/dynamo-commons-spring/src/main/java/com/shaw/spring/SpringConfigApplication.java
+++ dynamo-commons/dynamo-commons-spring/src/main/java/com/shaw/spring/SpringConfigApplication.java
@@ -1,13 +1,12 @@
 package com.shaw.spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@EnableRetry
+@ComponentScan
 public class SpringConfigApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringConfigApplication.class, args);
-	}
 
 }
