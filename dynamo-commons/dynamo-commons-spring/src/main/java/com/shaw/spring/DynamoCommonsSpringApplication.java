c6e5fa5ed11d181a5fe20704c962de4b8a729b3e
shaw <jind.shaw@outlook.com>
2023-07-11 18:23:07 +0800

    init code dynamo project

---
+++ dynamo-commons/dynamo-commons-spring/src/main/java/com/shaw/spring/DynamoCommonsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.shaw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoCommonsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoCommonsSpringApplication.class, args);
+	}
+
+}
