0eaf74e71a65d2ca8f83c42c93c283f1f34ab104
shaw <jind.shaw@outlook.com>
2023-07-12 18:34:20 +0800

    new cache module

---
+++ dynamo-commons/dynamo-commons-cache/src/main/java/com/shaw/cache/DynamoCommonsCacheApplication.java
@@ -0,0 +1,13 @@
+package com.shaw.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoCommonsCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoCommonsCacheApplication.class, args);
+	}
+
+}
---
+++ dynamo-commons/dynamo-commons-cache/src/test/java/com/shaw/cache/DynamoCommonsCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaw.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoCommonsCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
