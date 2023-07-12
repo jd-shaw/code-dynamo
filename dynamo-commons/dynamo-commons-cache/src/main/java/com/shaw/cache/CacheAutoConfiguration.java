0d6fcd878eb023c50b9c5f0403ad6911268ca2d7
shaw <jind.shaw@outlook.com>
2023-07-12 18:53:12 +0800

    new cache module

---
+++ dynamo-commons/dynamo-commons-cache/src/main/java/com/shaw/cache/CacheAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.shaw.cache;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@ConfigurationPropertiesScan
+@AutoConfiguration
+public class CacheAutoConfiguration {
+
+}
