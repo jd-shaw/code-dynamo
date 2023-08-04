424a5d910e217eead1e04dfc93667f70ae6aafe3
shaw <jind.shaw@outlook.com>
2023-08-04 14:25:45 +0800

    update monitor

---
+++ dynamo-services/dynamo-services-monitor/src/main/java/com/shaw/monitor/MonitorApplication.java
@@ -0,0 +1,13 @@
+package com.shaw.monitor;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author shaw
+ * @date 2023/8/4
+ */
+@ComponentScan
+@AutoConfiguration
+public class MonitorApplication {
+}
