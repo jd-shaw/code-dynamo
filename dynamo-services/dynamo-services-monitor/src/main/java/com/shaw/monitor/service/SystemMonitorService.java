40544a77ee96a94c72b8ba43a31460ad5c54054d
shaw <jind.shaw@outlook.com>
2023-08-11 17:06:06 +0800

    update monitor

--- dynamo-services/dynamo-services-monitor/src/main/java/com/shaw/monitor/service/SystemMonitorService.java
+++ dynamo-services/dynamo-services-monitor/src/main/java/com/shaw/monitor/service/SystemMonitorService.java
@@ -4,5 +4,5 @@ package com.shaw.monitor.service;
  * @author shaw
  * @date 2023/8/11
  */
-public class SystemMonitorService {
+public interface SystemMonitorService {
 }
---
+++ dynamo-services/dynamo-services-monitor/src/main/java/com/shaw/monitor/service/impl/SystemMonitorServiceImpl.java
@@ -0,0 +1,13 @@
+package com.shaw.monitor.service.impl;
+
+import com.shaw.monitor.service.SystemMonitorService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author shaw
+ * @date 2023/8/11
+ */
+@Service("systemMonitorService")
+public class SystemMonitorServiceImpl implements SystemMonitorService {
+
+}
